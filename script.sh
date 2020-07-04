b67881ab23f8fedf6a12601a46422bcaf6322ff9
Jenkins <jenkins@jenkins.example.com>
2020-07-04 14:24:01 -0400

    dont even ask...

--- script.sh
+++ script.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 docker run --rm  -v /home/jenkins/jenkins-data/jenkins_home/workspace/whyunkill/hello-world:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn -B -DskipTests clean package
-sudo chown -R jenkins:jenkins /home/jenkins/jenkins-data/jenkins_home/workspace/whyunkill/hello-world/server/target
-sudo chown -R jenkins:jenkins /home/jenkins/jenkins-data/jenkins_home/workspace/whyunkill/hello-world/webapp/target
