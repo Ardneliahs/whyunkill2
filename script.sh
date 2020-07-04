d86e0dad0966957db5ab854f3fb16bcea98be984
Jenkins <jenkins@jenkins.example.com>
2020-07-04 13:35:56 -0400

    changed script

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm  -v /home/jenkins/whyunkill/hello-world:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn -B -DskipTests clean package
+docker run --rm  -v /var/jenkins_home/workspace/whyunkill/hello-world:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn -B -DskipTests clean package
