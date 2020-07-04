aa94e66f51484806273d5b304c92c9fbe59617ca
Jenkins <jenkins@jenkins.example.com>
2020-07-04 13:10:31 -0400

    added ansible playbook to deploy on appserver

--- script.sh
+++ script.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 docker run --rm  -v /home/jenkins/whyunkill/hello-world:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn -B -DskipTests clean package
-docker-compose stop tomcat
-docker-compose -f docker-compose.yml up -d
