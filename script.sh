b8e606aaccef90ef3fbf779ab7559654ef76103a
Jenkins <jenkins@jenkins.example.com>
2020-07-05 01:25:25 -0400

    dont even askkksksks

--- script.sh
+++ script.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run --rm  -v /home/jenkins/jenkins-data/jenkins_home/workspace/whyunkill/hello-world:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn -B -DskipTests clean package
+mkdir hello-world
+cp -r workspace/whyunkill/hello-world hello-world
+docker run --rm  -v /home/jenkins/jenkins-data/jenkins_home/hello-world:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn -B -DskipTests clean package
