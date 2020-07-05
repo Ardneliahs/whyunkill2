ff4ac496bf77067176ce96190fb033a3702e7929
Jenkins <jenkins@jenkins.example.com>
2020-07-05 01:29:36 -0400

    dont even asafakjdhfladfjaskkksksks

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-mkdir hello-world
-cp -r workspace/whyunkill/hello-world hello-world
+#/var/jenkins_home/workspace/whyunkill/hello-world
+mkdir -p /var/jenkins_home/hello-world
+cp -r /var/jenkins_home/workspace/whyunkill/hello-world /var/jenkins_home/hello-world
 docker run --rm  -v /home/jenkins/jenkins-data/jenkins_home/hello-world:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn -B -DskipTests clean package
